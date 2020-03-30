740cf1459d4d5df42812f14fe7ce02ba465d969a
JakeSeib <jacob.a.seib@gmail.com>
2020-03-30 13:59:31 -0400

    Update quotes resource
    All fields are required
    Add title field

--- db/migrate/20200330131022_create_quotes.rb
+++ db/migrate/20200330131022_create_quotes.rb
@@ -1,10 +1,10 @@
 class CreateQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
-      t.string :text
-      t.string :author
-      t.string :cipher
-      t.integer :difficulty
+      t.string :text, null: false
+      t.string :author, null: false
+      t.string :cipher, null: false
+      t.integer :difficulty, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
